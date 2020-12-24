edcaf38685b2691cb62cd6553759bebc95f6c54d
y-solb <ysolb99@gmail.com>
2020-12-24 23:26:08 +0900

    navigation_tabbar

---
+++ app/src/main/java/com/yoon/solgram/navigation/AlarmFragment.kt
@@ -0,0 +1,15 @@
+package com.yoon.solgram.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yoon.solgram.R
+
+class AlarmFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
+            return view
+    }
+}
