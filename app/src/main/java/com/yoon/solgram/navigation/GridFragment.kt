937e9db5a2ca3c9c72c12506b6d1789f08a096c3
y-solb <ysolb99@gmail.com>
2021-01-13 14:58:24 +0900

    grid_page

--- app/src/main/java/com/yoon/solgram/navigation/GridFragment.kt
+++ app/src/main/java/com/yoon/solgram/navigation/GridFragment.kt
@@ -4,12 +4,70 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.firestore.FirebaseFirestore
 import com.yoon.solgram.R
+import com.yoon.solgram.navigation.model.ContentDTO
+import kotlinx.android.synthetic.main.fragment_grid.view.*
 
 class GridFragment : Fragment(){
+    var firestore : FirebaseFirestore? = null //전역변수
+    var fragmentView : View? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
-            return view
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_grid,container,false)
+        firestore = FirebaseFirestore.getInstance() //init
+        fragmentView?.gridfragment_recyclerview?.adapter = UserFragmentRecyclerViewAdapter()
+        fragmentView?.gridfragment_recyclerview?.layoutManager = GridLayoutManager(activity,3)
+            return fragmentView
+    }
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+
+        init { //db값 읽어오기
+            firestore?.collection("images")
+                ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    contentDTOs.clear()
+
+                    //Sometimes, this code returns null of querySnapshot when it signout
+                    if (querySnapshot == null)
+                        return@addSnapshotListener
+                    //Get data
+                    for (snapshot in querySnapshot.documents) {
+                        contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                    }
+                    notifyDataSetChanged() //RecyclerView 새로고침되도록
+                }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3 //폭의 1/3
+
+            var imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width, width) //폭의 1/3 정사각형
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView) :
+            RecyclerView.ViewHolder(imageView) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) { //데이터 매핑
+            var imageView = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl)
+                .apply(RequestOptions().centerCrop()).into(imageView) //이미지 다운로드
+        }
+
     }
 }
